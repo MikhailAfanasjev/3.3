9068db837dabf6b2408e7aaa95398738811ba6f8
MikhailAfanasjev <66779383+MikhailAfanasjev@users.noreply.github.com>
2020-07-02 18:39:16 +0300

    Add files via upload

---
+++ 3.3/src/ru/vsu/cs/course1/Main.java
@@ -0,0 +1,10 @@
+package ru.vsu.cs.course1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (String arg: args) {
+            System.out.println(arg);
+        }
+    }
+}
